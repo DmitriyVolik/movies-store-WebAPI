ced7dc3b79c7ce86625f17d74bb34a29530a6a3c
DmitriyVolik <dmitrvolik@gmail.com>
2022-04-27 21:10:32 +0300

    Add missed files

---
+++ WebAPI/Controllers/CommentsController.cs
@@ -0,0 +1,41 @@
+using BLL.Services;
+using Microsoft.AspNetCore.Mvc;
+using Models.DTO;
+using WebAPI.ActionFilters;
+
+namespace WebAPI.Controllers;
+
+[ApiController]
+[PerformanceActionFilter]
+[Route("[controller]")]
+public class CommentsController : ControllerBase
+{
+    private readonly CommentsService _commentsService;
+
+    public CommentsController(CommentsService moviesService)
+    {
+        _commentsService = moviesService;
+    }
+    
+    [HttpGet("{movieId}")]
+    public IEnumerable<CommentResponseDTO> Get(Guid movieId)
+    {
+        return _commentsService.GetCommentsByMovieId(movieId);
+    }
+
+    [HttpPost]
+    public IActionResult Post(CommentRequestDTO commentRequest)
+    {
+        // try
+        // {
+             _commentsService.AddComment(commentRequest);
+        // }
+        // catch (Exception e)
+        // {
+        //     return BadRequest(e.Message);
+        // }
+        
+        return Ok(commentRequest);
+    }
+
+}
